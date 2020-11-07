45b8798d795f39f029408b99597f3b94e0d61312
tejaswini <teju@PBC-SWD046304M.local>
2020-11-08 00:53:02 +0700

    Implementd edit and update functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-    before_action :set_post, only: [:show]
+    before_action :set_post, only: [:show, :edit]
 
     def index
         @posts = Post.all
@@ -18,6 +18,19 @@ class PostsController < ApplicationController
         else 
           render :new
         end  
+    end
+
+    def edit
+
+    end
+
+    def update
+      if  
+       @post.update(post_params)
+       redirect_to @post, notice: 'your post was created successfully'
+     else
+        render :edit
+     end     
     end
 
     def show 
